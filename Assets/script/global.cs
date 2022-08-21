2d9927b473447ab256f1abf27fc8b388ef4c8a4a
galaxy-comet <oleksandrshkabura811@gmail.com>
2022-08-21 22:27:20 +0800

    finial commit

---
+++ Assets/script/global.cs
@@ -0,0 +1,13 @@
+public class APIForm
+{
+    public int[] diceArray;
+    public string msg;
+    public float total;
+    public float raisePrice;
+    public string serverMsg;
+}
+
+public class Globalinitial
+{
+    public string BaseUrl = "http://192.168.115.183:443/";
+}
